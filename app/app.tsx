d0ebc0a1262c5018831d0e5bc192dd5ff5c3d75b
Navi <mailvoornavi@gmail.com>
2022-02-24 14:25:12 +0100

    added base

---
+++ app/app.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { IntlProvider } from 'react-intl';
+
+import App from './containers/App/App'
+
+const MOUNT_NODE = document.getElementById('app');
+
+const render = messages => {
+	ReactDOM.render(
+		<IntlProvider locale="en">
+			<App />
+		</IntlProvider>,
+		MOUNT_NODE,
+	);
+};
+
+render({});
