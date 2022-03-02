1b8ab786486bd26c13470e13dddfa789ca8424f3
Navi <mailvoornavi@gmail.com>
2022-03-02 23:44:54 +0100

    added axios example

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,20 +1,13 @@
-/**
- *
- * App
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
-
 import React from 'react'
 import styled from 'styled-components'
 
 import Container from '/src/components/Container'
+import PersonList from './PersonList'
 
 export default function App() {
 	return (
 		<Container>
-			Home
+			<PersonList/>
 		</Container>
 	)
 }
